88b31ce1d59d3ddf71927b96b9fafbf709f6ea5f
MainPC <alefebvre0510@gmail.com>
2024-02-15 15:52:45 -0500

    Empty Activity

---
+++ app/src/main/java/com/example/ift1155_projet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ift1155_projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
