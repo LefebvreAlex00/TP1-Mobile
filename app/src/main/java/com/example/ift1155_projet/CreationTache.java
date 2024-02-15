85df2bf920e72bb6626a6d84597047a80a550607
MainPC <alefebvre0510@gmail.com>
2024-02-15 18:14:32 -0500

    Base UI

--- app/src/main/java/com/example/ift1155_projet/CreationTache.java
+++ app/src/main/java/com/example/ift1155_projet/CreationTache.java
@@ -2,26 +2,27 @@ package com.example.ift1155_projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class CreationTache extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_creation_tache);
     }
 
-   public void creation(View v){
-       Intent i = new Intent(MainActivity.this,CreationTache.class);
-       startActivity(i);
-   }
+
+
+    public void fermer(View v){
+        Intent i = new Intent(CreationTache.this,MainActivity.class);
+        startActivity(i);
+    }
 
 }
